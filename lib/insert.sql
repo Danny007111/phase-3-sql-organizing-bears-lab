fb6fe70d59ed28194550da1f1cd7c93634ed5b20
Danny Rodriguez <danny007111@hotmail.com>
2022-07-31 16:03:53 -0500

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 40, "M", "milk", "calm", true);
+INSERT INTO bears VALUES (2, "Rowdy", 20, "M", "yellow", "aggressive", true);
+INSERT INTO bears VALUES (3, "Tabitha", 16, "F", "brown", "calm", true);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 73, "M", "blue", "calm", false);
+INSERT INTO bears VALUES (5, "Melissa", 19, "F", "brown", "calm", true);
+INSERT INTO bears VALUES (6, "Grinch", 30, "M", "green", "aggressive", true);
+INSERT INTO bears VALUES (7, "Wendy", 12, "F", "white", "calm", true);
+INSERT INTO bears VALUES (8, null, 20, "M", "white", "aggressive", false);
